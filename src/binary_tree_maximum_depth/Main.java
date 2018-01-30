cc7df84d51c4e1e80dab877c016f7b1e5c258326
triple <triple@192.168.10.10>
2018-01-30 16:15:02 +0800

    二叉树最大深度

---
+++ src/binary_tree_maximum_depth/Main.java
@@ -0,0 +1,37 @@
+package binary_tree_maximum_depth;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TreeNode root = new TreeNode(5);
+		root.left = new TreeNode(5);
+		System.out.println(maxDepth(root));
+	}
+	
+	  static public class TreeNode {
+	      int val;
+	      TreeNode left;
+	      TreeNode right;
+	      TreeNode(int x) { val = x; }
+	  }
+	  static public int maxDepth(TreeNode root) {
+		  if(root == null){
+	            return 0;
+	        }
+	        return ChildDepth(root);
+	    }
+	  static public int ChildDepth(TreeNode node){
+		  int leftDepth =0;
+	        int rightDepth =0;
+	        if(node.left != null){
+	            leftDepth = ChildDepth(node.left);
+	        }
+	        if(node.right != null){
+	            rightDepth = ChildDepth(node.right);
+	        }
+	        return (leftDepth > rightDepth ? leftDepth : rightDepth) + 1;
+	    }
+	public class Solution {
+	    
+	}
+}
